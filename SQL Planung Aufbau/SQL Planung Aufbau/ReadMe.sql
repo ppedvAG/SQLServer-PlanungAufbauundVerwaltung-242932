c0576d2019bc476ca46544ab582655cd16bd0df2
Rauchendraser <andreasr@ppedv.de>
2024-12-16 13:37:19 +0100

    Database und Serversettings

---
+++ SQL Planung Aufbau/SQL Planung Aufbau/ReadMe.sql
@@ -0,0 +1,36 @@
+/*
+
+Orga:
+
+9:00 -- 10:30 Teepause --- 12:45   13:45 --  15:00  -- 16:30 ..17..
+
+Tag 1
+Installation  best practice
+System DB
+DB (Settings)
+ DB Objekte Proc / Views / f()
+Architektur  Seiten und Blöcken
+ Agent Jobs/ Zeitpläne / Alerts / Operatoren
+ 
+
+Tag 2
+Backup / Restore
+Rechte (Logins/User)
+SQL Sprache
+
+Tag 3
+Monitoring
+Indizes und Stat -- Tuning
+Wartung
+Hochverfügbarkeit -- Ausflug
+Tools
+
+
+
+
+SSIS --
+
+
+
+
+*/
